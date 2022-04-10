0e1a8ca58d717c6ec420cb32e1fdd893880db72a
Tudor <30391543+TudorMurariu@users.noreply.github.com>
2022-04-10 14:16:44 +0300

    Create 2.sh

---
+++ Lab6/2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for i in $@
+do
+        if [ -f $i ]  # fisier
+        then
+                echo "Fisier :" $i
+                Nrl=$(grep -c "." $i)
+                echo "Numarul de linii este :" $Nrl
+
+
+        elif [ -d $i ]  # director
+        then
+                echo "Directorul :" $i
+                Nrf=$(find $i -type f | grep -c ".")
+                echo "Numarul de fisiere este :" $Nrf
+
+        else
+                echo "'$i' nu este nici fisier nici director"
+        fi
+done
+
+exit 0
